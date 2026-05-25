7f2d768ba26b207b23e9c6aebe5fd1c10532f5bb
Lfleck3504 <lfleck3504@gmail.com>
2026-05-25 15:09:19 -0400

    Update menu.js

--- services/menu.js
+++ services/menu.js
@@ -9,15 +9,4 @@ class MenuServices {
   }
 }
 
-module.exports = new MenuServices();const Menu = require("../models/menu");
-
-class MenuServices {
-  async getMenu(name = "") {
-    return await Menu.query()
-      .select("name", "price", "description")
-      .where("name", "ilike", `%${name}%`)
-      .orWhere("description", "ilike", `%${name}%`);
-  }
-}
-
 module.exports = new MenuServices();
